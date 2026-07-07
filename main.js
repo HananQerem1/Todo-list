78a59261b57f2d589f1ec63ace32aa0e5a7f302b
Hanan Qerem <hananqerem@gmail.com>
2026-07-07 18:32:11 +0300

    add empty state case

--- main.js
+++ main.js
@@ -21,26 +21,36 @@ addTaskForm.onsubmit = (e)=>{
 }
 
 const displayTasks = ()=> {
-    const result = tasks.map( (task,index)=> {
-        const isDone = task.isCompleted;
-        const icon = isDone? "./assets/img/icons/reset.svg":"./assets/img/icons/circle-check-mark.svg";
-        const imgAlt = isDone? "reset icon":"circle check mark icon";
-        const taskStatusBtnText = isDone? "undo":"completed";
-        return `<div class="${isDone? "bg-success-card": "bg-fg"} panel column">
-            <h2 class="${isDone? "line-through": ""}">${task.name}</h2>
-            <div class="row-end gap-4 w-full">
-                <button class="button-secondary row-start center bg-fg ${isDone? "text-warning":"text-success"}" onclick="taskStatus(${index})">
-                    <img src="${icon}" alt="${imgAlt}" />
-                    ${taskStatusBtnText}
-                </button>
-                <button class="button-secondary row-start center bg-fg text-danger" onclick="removeTask(${index})">
-                    <img src="./assets/img/icons/trash.svg" alt="trash icon" />
-                    delete
-                </button>
+    let result;
+    if(tasks.length === 0){
+            result = `<div class="empty-state column-center gap-3 m-auto">
+                <img src="./assets/img/icons/file.svg" alt="file icon" class="size-15" />
+                <p>no tasks yet. add one above!</p>
             </div>
-        </div>
-        `
-    }).join("");
+            `
+    } else{
+        result = tasks.map( (task,index)=> {
+            const isDone = task.isCompleted;
+            const icon = isDone? "./assets/img/icons/reset.svg":"./assets/img/icons/circle-check-mark.svg";
+            const imgAlt = isDone? "reset icon":"circle check mark icon";
+            const taskStatusBtnText = isDone? "undo":"completed";
+            
+            return `<div class="${isDone? "bg-success-card": "bg-fg"} panel column">
+                <h2 class="${isDone? "line-through": ""}">${task.name}</h2>
+                <div class="row-end gap-4 w-full">
+                    <button class="button-secondary row-start center bg-fg ${isDone? "text-warning":"text-success"}" onclick="taskStatus(${index})">
+                        <img src="${icon}" alt="${imgAlt}" />
+                        ${taskStatusBtnText}
+                    </button>
+                    <button class="button-secondary row-start center bg-fg text-danger" onclick="removeTask(${index})">
+                        <img src="./assets/img/icons/trash.svg" alt="trash icon" />
+                        delete
+                    </button>
+                </div>
+            </div>
+            `
+        }).join("");
+    }
     document.querySelector(".task-container").innerHTML = result;
     document.querySelector(".remainTaskNum").textContent = taskCount-doneTaskCount;
     document.querySelector(".doneTaskNum").textContent = doneTaskCount;
